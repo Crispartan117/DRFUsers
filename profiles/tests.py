e231111c4ed4d316c3f09b70036cd7e3bfd443e3
Cristian J. Ochoa <cjochoa@bomuca.com>
2019-07-11 17:51:20 -0700

    test case

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from profiles.api.serializers import ProfileSerializer
+from profiles.models import Profile
+
+
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+        data = {'username': 'testcase',
+                'email': 'test@local.com',
+                'password1': 'bomuca123',
+                'password2': 'bomuca123',}
+        response = self.client.post('/api/rest-auth/registration/', data)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+    
+class ProfileViewSetTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='davinci',
+                                             password='bomuca123')
+        self.token = Token.objects.create(user=self.user)
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION=)
